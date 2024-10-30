bcdc42b3c73b18d25a3efa2cf77b8bca2896c76e
Bellika <markusbellika@gmail.com>
2024-10-30 15:01:29 +0100

    changed strings to numerical values

--- mscan.py
+++ mscan.py
@@ -1,7 +1,10 @@
 import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('mushrooms.csv')
 
+
+# fixing missing value (now changing with most common, maybe remove column?)
 missing_stalk_root = data['stalk-root'] == '?'
 print('Number of missing values:', missing_stalk_root.sum())
 
@@ -12,5 +15,15 @@ print('Most common value in stalk root:', most_common_value)
 data['stalk-root'] = data['stalk-root'].replace('?', most_common_value)
 print(data['stalk-root'].value_counts())
 
+
+# transform strig values to numerical values
+label_encoders = {}
+for column in data.columns:
+  le = LabelEncoder()
+  data[column] = le.fit_transform(data[column])
+
+  label_encoders[column] = le
+
+print(f'\nTransformed data:\n {data.head}')
 
 
